89f99be501c96db00edf2cb716accefe9bb46a6f
Aarav Mathur <78998762+13100D@users.noreply.github.com>
2024-07-29 18:26:02 +0530

    Update BasicBlockCoverage.java
    minor fix lol

--- basicBlockCoverage/src/main/java/de/uni_passau/fim/auermich/instrumentation/basicblockcoverage/BasicBlockCoverage.java
+++ basicBlockCoverage/src/main/java/de/uni_passau/fim/auermich/instrumentation/basicblockcoverage/BasicBlockCoverage.java
@@ -167,7 +167,7 @@ public class BasicBlockCoverage {
             }
 
             // the name of the instrumented APK
-            File outputAPKFile = new File(apkPath.getParentFile(), apkPath.getName() + "-instrumented.apk");
+            File outputAPKFile = new File(apkPath.getParentFile(), apkPath.getName().substring(0, apkPath.getName().length() - 4) + "-instrumented.apk");
 
             // build the instrumented APK together
             boolean builtAPK = Utility.buildAPK(decodedAPKPath, outputAPKFile);
